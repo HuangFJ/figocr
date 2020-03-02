db5f6846770d70f9290a06d87873372342687a83
Jon Huang <biohfj@gmail.com>
2020-03-02 22:52:34 +0800

    init

---
+++ src/procedure/image.py
@@ -0,0 +1,483 @@
+# -*- coding: utf8 -*-
+import cv2
+import numpy as np
+from skimage.morphology import opening, closing, square
+from skimage.filters import threshold_otsu
+import logging
+
+
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    return resized
+
+
+def cv2_imshow(image):
+    import matplotlib.pyplot as plt
+    plt.imshow(image, interpolation = 'bicubic')
+    plt.show()
+    # cv2.imshow('debug', image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    # cv2.waitKey(1)
+
+
+def roi_detect(image, region, thresh_mean=None, trace=False):
+    # (x, y, w, h)
+    x, y, width, height = region
+    
+    up_y_offset = int(height / 2)
+    up_y = y - up_y_offset
+    down_y_offset = height + int(height / 2)
+    down_y = y + down_y_offset
+
+    roi = image[up_y : down_y, x : x + width]
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    
+    try:
+        thresh_value = int(1*threshold_otsu(gray))
+    except:
+        thresh_value = 255
+
+    if thresh_mean is not None:
+        thresh_value = min(thresh_value, thresh_mean)
+    thresh = cv2.threshold(gray, thresh_value, 255, cv2.THRESH_BINARY_INV)[1]
+    thresh = closing(thresh)
+    #垂直计算留白
+    yitensity = np.sum(thresh, axis=1)
+
+    middle = height
+    step = 1
+    while yitensity[middle] == 0:
+        middle = height + step
+        if step < 0:
+            step = abs(step)+1
+        else:
+            step = - step
+
+        if abs(step) > 20:
+            # 行中间水平线上下20个单位内没有内容
+            return None
+
+    # 上边距4个空白单位
+    up_blank_line = 0
+    for i in reversed(range(middle)):
+        if yitensity[i] == 0:
+            up_y_offset = i
+            up_blank_line += 1
+        if up_blank_line > 3:
+            break
+    # 下边距4个空白单位
+    down_blank_line = 0
+    for i in range(middle, (down_y - up_y)):
+        if yitensity[i] == 0:
+            down_y_offset = i
+            down_blank_line += 1
+        if down_blank_line > 3:
+            break
+    
+    y = up_y + up_y_offset
+    height = down_y_offset - up_y_offset
+
+    # 垂直裁剪
+    thresh = thresh[up_y_offset : down_y_offset, 0 : width]
+    thresh = cv2.Canny(thresh, 100, 200, 3, L2gradient=True)
+    thresh = cv2.dilate(thresh, None)
+    # 水平计算留白
+    xitensity = np.sum(thresh, axis=0)
+
+    x_offset = 0
+    x_suffix = len(xitensity) - 1
+    while True:
+        if (x_offset >= x_suffix) or (xitensity[x_offset] and xitensity[x_suffix]):
+            break
+
+        if xitensity[x_offset] == 0:
+            x_offset += 1
+        if xitensity[x_suffix] == 0:
+            x_suffix -= 1
+
+    x_offset = x_offset - 5 if x_offset - 5 > 0 else 0
+    x_suffix = x_suffix + 5 if x_suffix + 5 < len(xitensity) else (len(xitensity) - 1)
+
+    x = x + x_offset
+    width = x_suffix - x_offset
+
+    if height < 16 or width <= 10:
+        # 行内容高度只有8个单位（小数点的大小）
+        return None
+
+    # # 水平裁剪
+    # thresh = thresh[0 : height, x_offset : x_suffix]
+
+    # cv2.rectangle(image, (x+cnt_x, y+cnt_y), (x+cnt_x + cnt_w-2, y+cnt_y + cnt_h-2), (0, 0, 0), 1)
+    # cv2.rectangle(image, (x, y), (x + width-2, y + height-2), (0, 255, 0), 1)
+    return x, y, width, height
+    
+
+def max_width_poly(image, region, thresh_mean=None):
+    x, y, width, height = region
+    
+    roi = image[y : y + height, x : x + width]
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    
+    try:
+        thresh_value = int(1*threshold_otsu(gray))
+    except:
+        thresh_value = np.percentile(gray, 50)
+    if thresh_mean is not None:
+        thresh_value = min(thresh_value, thresh_mean)
+    thresh = cv2.threshold(gray, thresh_value, 255, cv2.THRESH_BINARY_INV)[1]
+
+    thresh = cv2.Canny(thresh, 100, 200, 3, L2gradient=True)
+    thresh = cv2.dilate(thresh, None)
+
+    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+    boxes = map(lambda cnt: cv2.boundingRect(cnt), cnts)
+
+    boxes = sorted(boxes, key=lambda x: x[2])
+    if boxes:
+        box = boxes.pop()
+        return x+box[0], y+box[1], box[2], box[3]
+    else:
+        return None
+
+
+def threshold(image):
+    return int(1*threshold_otsu(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)))
+
+
+def consine(pt1, pt2, pt0):
+    v1 = np.array(pt1) - np.array(pt0)
+    v2 = np.array(pt2) - np.array(pt0)
+    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
+
+def angle_degree(pt1, pt2, pt0):
+    radian = np.arccos(consine(pt1, pt2, pt0))
+    return np.degrees(radian)
+
+
+def square_contours_kps(image, min_area=1800, min_density=None):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    edges = cv2.Canny(gray, 0, 0)
+
+    # linesP = cv2.HoughLinesP(edges, 1, (np.pi / 180)*10, 50, None, 100, 10)
+    # if linesP is not None:
+    #     for i in range(0, len(linesP)):
+    #         l = linesP[i][0]
+    #         cv2.line(gray, (l[0], l[1]), (l[2], l[3]), (0,0,255), 1, cv2.LINE_AA)
+    # cv2_imshow(gray)
+    
+    edges = closing(edges, square(3))
+    # cv2_imshow(edges)
+    cnts, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    squares = []
+    for cnt in cnts:
+        approx = cv2.approxPolyDP(cnt, cv2.arcLength(cnt, True)*0.02, True)
+        area = cv2.contourArea(approx)
+
+        # cv2.drawContours(image, [approx], -1, (0,0,255), 1) 
+
+        if area < min_area:
+            continue
+
+        # cv2.drawContours(image, [approx], -1, (0,0,255), 1) 
+        # print(f'area: {area}, approx: {approx}')
+
+        # for v in approx:
+        #     cv2.circle(image, tuple(v[0]), 3, (0, 255, 255), -1)
+
+        vertex_num = len(approx)
+        if vertex_num >= 4: #4个或多于4个顶点 and cv2.isContourConvex(approx):
+            for offset in range(0, vertex_num): #取连续的4个顶点
+                square_approx = []
+                for idx in range(0, 4):
+                    sq_index = (offset+idx) % vertex_num
+                    square_approx.append(approx[sq_index])
+
+                square_approx = np.array(square_approx)
+                maxCosine = 0
+                for j in range(2, 5):
+                    square_pts = np.squeeze(square_approx)
+                    cosine = abs(consine(square_pts[j%4], square_pts[j-2], square_pts[j-1]))
+                    # print(cosine)
+                    maxCosine = max(maxCosine, cosine)
+
+                if maxCosine < 0.20: # up and down 12 degree
+                    if vertex_num > 4:
+                        area = cv2.contourArea(square_approx)
+                        if area < min_area:
+                            continue
+
+                    mask = np.zeros(edges.shape, dtype="uint8")
+                    cv2.drawContours(mask, [square_approx], -1, 255, -1)
+                    mask = cv2.bitwise_and(edges, edges, mask=mask)
+                    # cv2_imshow(mask)
+                    mass = cv2.countNonZero(mask)
+                    density = mass / area
+                    logging.info(f'area:{area}, mass:{mass}')
+                    if min_density is not None:
+                        if density < min_density:
+                            continue
+
+                    squares.append((area, density, square_approx))
+                    break
+
+    # cv2.drawContours(image, np.array([sq[1] for sq in squares]), -1, (0,0,255), 1) 
+    # cv2_imshow(image)
+    if len(squares) < 4:
+        return None
+    
+    # sort by area
+    squares.sort(key=lambda sq: -sq[0])
+    squares = squares[:4]
+
+    result = []
+    # (area, density, contour)
+    for _, _, sq in squares:
+        M = cv2.moments(sq)
+
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+
+        result.append([cx, cy])
+
+    return result
+
+
+def block_contours_kps(image, min_area=1800):
+    # convert it to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # cv2_imshow(gray)
+
+    result = None
+    delta = 2
+    while result is None and delta < 5: #O(3)
+        # blur it slightly
+        ksize = 2 * delta + 1
+        blur = cv2.GaussianBlur(gray, (ksize, ksize), delta)
+        # cv2_imshow(blur)
+
+        # threshold the image 
+        thresh_value = threshold_otsu(blur)
+        thresh_factor = 3
+
+        while result is None and thresh_factor > 0: #O(3*3)
+            thresh = cv2.threshold(blur, thresh_value/thresh_factor, 255, cv2.THRESH_BINARY_INV)[1]
+            # perform a series of erosions + dilations to remove any small regions of noise
+            thresh = cv2.erode(thresh, None, iterations=2)
+            thresh = cv2.dilate(thresh, None, iterations=2)
+            # cv2_imshow(thresh)
+
+            # find contours in thresholded image
+            cnts, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            # image_copy = image.copy()
+            # cv2.drawContours(image_copy, cnts, -1, (0,0,255), 2) 
+            # cv2_imshow(image_copy)
+            
+            # 没有连通域形状
+            if len(cnts) < 4:
+                thresh_factor -= 1
+                continue
+            
+            cnts = [(cnt, cv2.moments(cnt)) for cnt in cnts]
+            cnts = [cnt for cnt in cnts if cnt[1]['m00'] > min_area/delta]
+            # image_copy = image.copy()
+            # cv2.drawContours(image_copy, [cnt[0] for cnt in cnts], -1, (0,255,0), 1) 
+            # cv2_imshow(image_copy)
+
+            # 找到形状面积超过设定值的形状
+            if len(cnts) < 4:
+                thresh_factor -= 1
+                continue
+
+            cnts = sorted(cnts, key=lambda cnt: -cnt[1]['m00'])
+            # 黑色面积最大的四个形状
+            cnts_target = cnts[:4]
+
+            result = []
+            for cnt, M in cnts_target:
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+
+                # M['m00'] is the mass, [cx, cy] is the centroid
+                result.append([cx, cy])
+
+            # image_copy = image.copy()
+            # for index, (cnt, M) in enumerate(cnts_target):
+            #     print(M['m00'])
+            #     cv2.drawContours(image_copy, [cnt], -1, (0,255,0), 1) 
+            #     cv2.circle(image_copy, tuple(result[index]), 5, (255, 255, 255), -1)
+            # cv2_imshow(image_copy)
+
+        delta += 1
+    
+    logging.info(f'delta: {delta}, thresh factor: {thresh_factor}')
+    return result
+
+
+def get_square_vertex(kps):
+    """ 
+    pt: [x, y]
+    kps: [ pt1, pt2, pt3, pt4 ]
+    return: [ top, right, bot, left ]
+    """
+    if not kps or len(kps) != 4:
+        return None
+
+    kps.sort(key=lambda p: p[0])
+    left_p = kps[:2]
+    right_p = kps[2:]
+
+    left_p.sort(key=lambda p: p[1])
+    extTop, extLeft = left_p
+    right_p.sort(key=lambda p: p[1])
+    extRight, extBot = right_p
+
+    # rows,cols,_ = image.shape
+
+    # angle_horizon = np.arctan2(extRight[1] - extTop[1], extRight[0] - extTop[0])
+    # deg = np.rad2deg(angle_horizon)
+
+    # M = cv2.getRotationMatrix2D((cols/2,rows/2),deg,1)
+    # dst = cv2.warpAffine(image,M,(cols,rows))
+    # cv2_imshow(dst)
+    degrees = [
+        angle_degree(extRight, extLeft, extTop), 
+        angle_degree(extTop, extBot, extRight), 
+        angle_degree(extRight, extLeft, extBot), 
+        angle_degree(extBot, extTop, extLeft)
+    ]
+    degree_max = max(degrees)
+    degree_min = min(degrees)
+    # print(degree)
+    if 89 <= degree_min and degree_max <= 91:
+        return np.array([extTop, extRight, extBot, extLeft])
+    else:
+        return None
+
+
+def draw_match_2_side(img1, kp1, img2, kp2, N):
+    """Draw matches on 2 sides
+    Args:
+        img1 (HxW(xC) array): image 1
+        kp1 (Nx2 array): keypoint for image 1
+        img2 (HxW(xC) array): image 2
+        kp2 (Nx2 array): keypoint for image 2
+        N (int): number of matches to draw
+    Returns:
+        out_img (Hx2W(xC) array): output image with drawn matches
+    """
+    kp_list = np.linspace(0, min(kp1.shape[0], kp2.shape[0])-1, N, dtype=np.int)
+
+    # Convert keypoints to cv2.Keypoint object
+    cv_kp1 = [cv2.KeyPoint(x=pt[0], y=pt[1], _size=1) for pt in kp1[kp_list]]
+    cv_kp2 = [cv2.KeyPoint(x=pt[0], y=pt[1], _size=1) for pt in kp2[kp_list]]
+
+    out_img = np.array([])
+    good_matches = [cv2.DMatch(_imgIdx=0, _queryIdx=idx, _trainIdx=idx,_distance=0) for idx in range(N)]
+    out_img = cv2.drawMatches(img1, cv_kp1, img2, cv_kp2, matches1to2=good_matches, outImg=out_img)
+
+    return out_img 
+
+
+class Image(object):
+
+    frames = {}
+
+    @classmethod
+    def get_frame(cls, frame_file):
+        frame = cls.frames.get(frame_file)
+        if frame is None:
+            img = cv2.imread(frame_file)
+            vertexs = get_square_vertex(block_contours_kps(img))
+            if vertexs is None:
+                return None
+            frame = (img, vertexs)
+            cls.frames[frame_file] = frame
+
+        return frame
+    
+    @classmethod
+    def get_image(cls, image_file):
+        img = cv2.imread(image_file)
+        vertexs = get_square_vertex(block_contours_kps(img))
+        if vertexs is None:
+            vertexs = get_square_vertex(square_contours_kps(img))
+
+        if vertexs is not None:
+            return (img, vertexs)
+        else:
+            return None
+
+    @classmethod
+    def align_images(cls, image_file, frame_file):
+        # scanned image
+        image_info = cls.get_image(image_file)
+        if not image_info:
+            return None
+        img, kps = image_info
+
+        # template image
+        mask_info = cls.get_frame(frame_file)
+        if not mask_info:
+            return None
+        mask_img, mask_kps = mask_info
+        
+        # Draw top matches
+        image = draw_match_2_side(img, kps, mask_img, mask_kps, 4)
+
+        ####### DEBUG #########
+        # # resize image
+        # image = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
+        # cv2_imshow(image)
+        ####### DEBUG #########
+
+        # Find homography
+        m, mask = cv2.findHomography(kps, mask_kps, cv2.RANSAC, 5.0)
+
+        # Use homography to warp image
+        h, w, _ = mask_img.shape
+        result = cv2.warpPerspective(img, m, (w, h))
+
+        return result
+
+
+if __name__ == '__main__':
+    logging.root.setLevel(logging.INFO)
+    
+    bads = [
+        '/Users/jon/Documents/cv/data/CM-MBL-E-01/OCR20170828_0020.tif',
+        '/Users/jon/Documents/cv/data/CM-MBL-E-01/OCR2017091_0010.tif'
+    ]
+    goods = [
+        '/Users/jon/Documents/cv/data/CM-MBL-E-01/CCE2017068_0008.tif',
+        '/Users/jon/Documents/cv/data/CM-MBL-E-01/CCE2017068_0009.tif'
+    ]
+    for i in bads:
+        image, vertexs = Image.get_image(i)
+        extTop, extRight, extBot, extLeft = vertexs
+        ####### DEBUG #########
+        cv2.circle(image, tuple(extLeft), 2, (0, 0, 255), -1)
+        cv2.circle(image, tuple(extRight), 2, (0, 255, 0), -1)
+        cv2.circle(image, tuple(extTop), 2, (255, 0, 0), -1)
+        cv2.circle(image, tuple(extBot), 2, (255, 255, 0), -1)
+        cv2_imshow(image)
+        ####### DEBUG #########
---
+++ src/sketchs/carthage/ocr.py
@@ -0,0 +1,301 @@
+# -*- coding: utf8 -*-
+from ..base import Base
+from collections import OrderedDict
+import logging
+from pathlib import Path
+import numpy as np
+import subprocess
+from xml.dom import minidom
+import re
+from procedure.image import Image, threshold, roi_detect, max_width_poly, cv2_imshow
+import cv2
+
+
+class OCR(Base):
+
+    def __init__(self, model_path, dataset_dir):
+        self.data_dir = Path(__file__).parent.joinpath('data')
+        self.dataset_dir = dataset_dir
+    
+    def parse_target_csv(self, csv_file):
+        """
+        {
+            0: {'farm':'', 'week':'', 'date':''},
+            1: { // row 1
+                1:'', // column 1
+                2:'', // column 2
+                3:'', // column 3
+                ...
+            }, 
+            2: { // row 2
+                1:'', // column 1
+                2:'', // column 2
+                3:'', // column 3
+                ...
+            }, 
+            ...
+        }
+        """
+        result = {}
+        with open(csv_file, 'r') as f:
+            result[0] = dict(zip(['farm','week','date'], map(lambda x: x.strip(), f.readline().rstrip(' ,\n').split(','))))
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                row_data = line.rstrip().split(',')
+                row_num = int(row_data[0])
+                other = row_data[1:]
+                if other:
+                    result[row_num] = {}
+                    for i, cell in enumerate(other):
+                        result[row_num][i+1] = cell.strip()
+        return result
+
+    def parse_frame_svg(self, svg_file):
+        """
+        {
+            'farm': [x, y, width, height],
+            'farm:expr': farm_expr,
+            'week': [x, y, width, height],
+            'week:expr': week_expr,
+            'date': [x, y, width, height],
+            'date:expr': date_expr,
+            'y_scales': [y1, y2, ...],
+            'x_scales': [x1, x2, ...],
+            'x_scales:expr': [x1_expr, x2_expr, ...],
+        }
+        """
+        result = {
+            'farm': None,
+            'farm:expr': None,
+            'week': None,
+            'week:expr': None,
+            'date': None,
+            'date:expr': None,
+            'y_scales': None,
+            'x_scales': None,
+            'x_scales:expr': None,
+        }
+        root = minidom.parse(svg_file).documentElement
+        for node in root.childNodes:
+            if node.nodeName == 'g':
+                for node in node.childNodes:
+                    if node.nodeName == 'rect':
+                        rect = np.array([node.attributes['x'].value,
+                                        node.attributes['y'].value,
+                                        node.attributes['width'].value,
+                                        node.attributes['height'].value]).astype(float).astype(int)
+                        expr = node.attributes['id'].value
+                        if 'farm' in expr:
+                            key = 'farm'
+                        elif 'week' in expr:
+                            key = 'week'
+                        elif 'date' in expr:
+                            key = 'date'
+                        else:
+                            continue
+                        result[key] = rect
+                        result[f'{key}:expr'] = expr
+                    elif node.nodeName == 'g':
+                        identity = node.attributes['id'].value
+                        scales = []
+                        transform = np.array(node.attributes['transform'].value[10:-1].split(',')).astype(float).astype(int)
+                        for node in node.getElementsByTagName('path'):
+                            m=re.search(r'M(\d+),(\d+)', node.attributes['d'].value)
+                            scales.append((
+                                transform + np.array([m.group(1),m.group(2)]).astype(float).astype(int),
+                                node.attributes['id'].value
+                            ))
+                        
+                        if identity == 'horizon':
+                            scales = [s[0][1] for s in scales]
+                            scales.sort()
+                            result['y_scales'] = scales
+                        else:
+                            scales.sort(key=lambda x: x[0][0])
+                            result['x_scales'] = [s[0][0] for s in scales]
+                            result['x_scales:expr'] = [s[1] for s in scales]
+
+                break
+        
+        return result
+    
+    def guess_frame_name(self, image_file):
+        out = b''
+        with subprocess.Popen(["tesseract", f'{image_file}', "-", "-l", "eng"], stdout=subprocess.PIPE) as proc:
+            proc.wait()
+            out = proc.stdout.read()
+            proc.stdout.close()
+
+        if b'WEAN' in out:
+            return 'CM-MWL-E-01'
+        elif b'PIGLET' in out:
+            return 'CM-PML-E-01'
+        elif b'TREATMENT' in out:
+            return 'CM-TEL-E-00'
+        elif b'FARROW' in out:
+            return 'CM-MFL-E-01'
+        elif b'MASTER BREEDING' in out:
+            return 'CM-MBL-E-01'
+        elif b'STOCK' in out:
+            return 'CM-BSR-E-00'
+        elif b'BREEDING EVENTS' in out:
+            return 'CM-BEL-E-00'
+        else:
+            return None
+        
+    def recognize(self, image_file, frame_name=None):
+        # ######### for training ############
+        # dataset_list = []
+        # ######### for training ############
+
+        if frame_name is None:
+            frame_name = self.guess_frame_name(image_file)
+
+        if not frame_name:
+            logging.error(image_file)
+            raise 'please specific a frame name'
+
+        svg_file = self.data_dir.joinpath(f'{frame_name}.svg')
+        frame_file = self.data_dir.joinpath(f'{frame_name}.svg')
+        meta = self.parse_frame_svg(svg_file)
+
+        output_dir = Path(image_file).parent
+        image_name = Path(image_file).name
+
+        box_file = output_dir.joinpath(f'{image_name[:-4]}.box.png')
+        csv_file = output_dir.joinpath(f'{image_name[:-4]}.csv')
+        # ######### for training ############
+        # target_data = self.parse_target_csv(csv_file)
+        # ######### for training ############
+
+        image = Image.align_images(image_file, frame_file)
+        if image is None:
+            logging.error(image_file)
+            raise f'the input image {image_file} procedure fail'
+        image_copy = image.copy()
+        
+        table = image[meta['y_scales'][0] : meta['y_scales'][-1], meta['x_scales'][0] : meta['x_scales'][-1]]
+        thresh = threshold(table)
+
+        farm = roi_detect(image, tuple(meta['farm']), thresh)
+        week = roi_detect(image, tuple(meta['week']), thresh)
+        date = roi_detect(image, tuple(meta['date']), thresh)
+
+        table_boxes = OrderedDict({
+            0: {'farm': farm, 'week': week, 'date': date}
+        })
+
+        for j in range(len(meta['y_scales'])-1):
+            cell_y = meta['y_scales'][j]
+            cell_height = meta['y_scales'][j + 1] - meta['y_scales'][j]
+            
+            row_rect = (int(meta['x_scales'][0]), int(cell_y), int(meta['x_scales'][-1] - meta['x_scales'][0]), int(cell_height))
+            max_poly = max_width_poly(image, row_rect, thresh)
+            if max_poly is None:
+                continue
+
+            # cv2.rectangle(image_copy, (max_poly[0], max_poly[1]), (max_poly[0]+max_poly[2], max_poly[1]+max_poly[3]), (0, 255, 0), 1)
+
+            row = OrderedDict({})
+            row_nul_cell = 0
+            max_cell = 0
+            for i in range(len(meta['x_scales'])-1):
+                cell_x = meta['x_scales'][i]
+                cell_width = meta['x_scales'][i + 1] - meta['x_scales'][i]
+                rect = (int(cell_x), int(cell_y), int(cell_width), int(cell_height))
+                row[i+1] = roi_detect(image, rect, thresh)
+                
+                if row[i+1] is None:
+                    row_nul_cell += 1
+                
+                if cell_width > max_cell:
+                    max_cell = cell_width
+            
+            if max_poly[2] > max_cell:
+                continue
+            if row_nul_cell == len(row):
+                continue
+
+            table_boxes[j+1] = row
+
+        csv_data = []
+        for row_num, row in table_boxes.items():
+            if row:
+                row_data = []
+                if row_num > 0:
+                    row_data = [row_num]
+                csv_data.append(row_data)
+
+                for column, cell in row.items():
+                    row_data.append('')
+                    
+                    if row_num > 0:
+                        expr = meta['x_scales:expr'][column-1]
+                        if expr.startswith('set') or expr.startswith('nil'):
+                            continue
+                    if cell:
+                        roi_target = 255 - image[cell[1]: cell[1] + cell[3], cell[0]: cell[0] + cell[2]]
+
+                        # ######### for training ############
+                        # datum = target_data[row_num][column]
+                        # datum_file = f'{image_name}-{row_num}-{column}_{datum}.png'
+                        # cv2.imwrite(f'{self.dataset_dir}/images/{datum_file}', roi_target)
+                        # dataset_list.append(f'{datum_file}\t{datum}')
+                        # ######### for training ############
+                        
+                        cv2.rectangle(image_copy, (cell[0], cell[1]), (cell[0]+cell[2], cell[1]+cell[3]), (0, 0, 255), 1)
+                        
+                        normal_roi = ocr_dataset.normalize(roi_target)
+                        normal_roi = to_tensor(normal_roi)
+                        # print(normal_roi.shape)
+                        # cv2_imshow(np.transpose(normal_roi*255, (1, 2, 0)).numpy())
+                        # model = Model(len(characters), input_shape=normal_roi.shape).cuda()
+                        # model = torch.nn.DataParallel(model, device_ids=range(1))
+                        # model.load_state_dict(ocr_state_dict)
+                        output = ocr(normal_roi.unsqueeze(0).cuda())
+                        # print(output)
+                        output_argmax = output.detach().permute(1, 0, 2).argmax(dim=-1)
+                        pred = decode(output_argmax[0])
+
+                        row_data[-1]=pred
+                        # cv2.putText(image_copy, pred, (cell[0], cell[1] - 3), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+        
+        # image_copy = cv2.resize(image_copy, None, fx=0.7, fy=0.7, interpolation = cv2.INTER_CUBIC)
+        # cv2_imshow(image_copy)
+        cv2.imwrite(box_file, image_copy)
+        with open(csv_file, 'w') as csv:
+            for row in csv_data:
+                csv.write(','.join(map(lambda x:str(x), row)) + '\n')
+
+        # ######### for training ############
+        # with open(f'{self.dataset_dir}/list.txt', 'a') as list_file:
+        #     list_file.writelines(dataset_list)
+        # ######### for training ############
+
+
+if __name__ == '__main__':
+    import os
+
+    dirs = [
+        "/content/drive/My Drive/cv/images/CM-BEL-E-00",
+        "/content/drive/My Drive/cv/images/CM-BSR-E-00",
+        "/content/drive/My Drive/cv/images/CM-MBL-E-01",
+        "/content/drive/My Drive/cv/images/CM-MFL-E-01",
+        "/content/drive/My Drive/cv/images/CM-MWL-E-01",
+        "/content/drive/My Drive/cv/images/CM-PML-E-01",
+        "/content/drive/My Drive/cv/images/CM-TEL-E-00"
+    ]
+    image_num = 0
+    for data_dir in dirs:
+        template_name = data_dir[-11:]
+
+        for _, _, filenames in os.walk(data_dir):
+
+            for filename in filenames:
+                # image_num += 1
+                # if image_num > 2:
+                #     break
+
+                image_file = f'{data_dir}/{filename}'
